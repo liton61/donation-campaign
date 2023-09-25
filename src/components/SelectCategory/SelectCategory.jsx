91740d0e57b0150c6adcde3a24c496c257ef8182
liton61 <litonofficial61@gmail.com>
2023-09-25 18:06:32 +0600

    local storage successfully connected

--- src/components/SelectCategory/SelectCategory.jsx
+++ src/components/SelectCategory/SelectCategory.jsx
@@ -10,7 +10,18 @@ const SelectCategory = () => {
         const findCategory = cards?.find(card => card.id == id);
         setCategory(findCategory);
     }, [id, cards]);
-    console.log(category);
+    const handleAddToDonation = () => {
+        const addDonationArray = [];
+        const donation = JSON.parse(localStorage.getItem('donation'));
+        if (!donation) {
+            addDonationArray.push(category)
+            localStorage.setItem('donation', JSON.stringify(addDonationArray))
+        }
+        else {
+            addDonationArray.push(...donation, category)
+            localStorage.setItem('donation', JSON.stringify(addDonationArray))
+        }
+    }
 
     return (
         <div>
@@ -19,8 +30,8 @@ const SelectCategory = () => {
                     <img src={category.img} alt="Banner Image" className="w-full h-screen" />
                     <div className="absolute inset-x-0 bottom-0 flex items-center justify-between h-1/6 bg-black opacity-50">
                     </div>
-                    <button className="absolute bottom-4 left-4-4 bg-[#FF444A] text-white font-semibold py-2 px-4 ml-10 mb-4">
-                    Donate $290
+                    <button onClick={handleAddToDonation} className="absolute bottom-4 left-4-4 bg-[#FF444A] text-white font-semibold py-2 px-4 ml-10 mb-4">
+                        Donate ${category.price}
                     </button>
                 </div>
 
@@ -28,10 +39,10 @@ const SelectCategory = () => {
 
                 <div className="p-6">
                     <h4 className="block font-sans text-2xl font-semibold leading-snug tracking-normal text-black antialiased">
-                        Good Education
+                        {category.title}
                     </h4>
                     <p className="mt-3 block font-sans text-l text-justify font-normal leading-relaxed text-gray-700 antialiased">
-                        There are many things that can be done to ensure that all people have access to a good education. Governments can invest in public schools, provide financial assistance to students, and make sure that all schools have qualified teachers and resources. Families can support their children education by creating a learning environment at home and helping them with their schoolwork. Teachers can create a positive and supportive learning environment for their students and challenge them to reach their full potential.
+                        {category.description}
                     </p>
                 </div>
             </div>
